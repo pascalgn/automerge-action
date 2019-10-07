8ab5758209042c1eb688c3339d3090a67710bf72
Pascal <pascal.gruen@gmail.com>
2019-10-07 21:12:49 +0200

    Fix tests

--- test/update.test.js
+++ test/update.test.js
@@ -5,7 +5,7 @@ const { pullRequest } = require("./common");
 test("update will only change branches from the same repository", async () => {
   const pr = pullRequest();
   pr.head.repo.full_name = "other/repository";
-  await expect(update(null, "", "", pr)).rejects.toEqual(
+  await expect(update({ config: {} }, "", "", pr)).rejects.toEqual(
     new NeutralExitError()
   );
 });
