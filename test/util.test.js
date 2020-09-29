dc42a433ae96c4eac117e39fc9feb1b57fbebab2
Pascal <pascal.gruen@gmail.com>
2020-09-29 22:51:08 +0200

    Update formatting

--- test/util.test.js
+++ test/util.test.js
@@ -3,7 +3,9 @@ const { branchName } = require("../lib/util");
 describe("branchName", () => {
   it("returns the branch name from a reference referring to a branch", async () => {
     expect(branchName("refs/heads/main")).toEqual("main");
-    expect(branchName("refs/heads/features/branch_with_slashes")).toEqual("features/branch_with_slashes");
+    expect(branchName("refs/heads/features/branch_with_slashes")).toEqual(
+      "features/branch_with_slashes"
+    );
   });
 
   it("is falsey for other kinds of git references", async () => {
